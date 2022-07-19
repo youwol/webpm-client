ea4581cb6339f91cfe4f7da0c804cb6b23136e7d
greinisch <greinisch@youwol.com>
2022-07-19 16:03:33 +0200

    🔧 yw_pipeline => add links doc & bundle-analysis & html-outputs + fix coverage link

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -2,13 +2,13 @@ from youwol.environment.forward_declaration import YouwolEnvironment
 from youwol.environment.models import IPipelineFactory
 from youwol.environment.models_project import JsBundle, Artifact, FileListing, Link
 from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig, TestStepConfig, test_result, \
-    test_coverage
+    test_coverage, PublishConfig
 from youwol_utils.context import Context
 
 test_html_outputs: Artifact = Artifact(
     id='test-html-outputs',
     files=FileListing(
-        include=["src/tests/.html-outputs"],
+        include=["src/tests/.html-outputs/**/*"],
     ),
     links=[
         Link(
@@ -26,9 +26,17 @@ class PipelineFactory(IPipelineFactory):
 
     async def get(self, env: YouwolEnvironment, context: Context):
         config = PipelineConfig(
-            target=JsBundle(links=[Link(name="coverage", url="/dist/coverage/l-cov")]),
+            target=JsBundle(links=[
+                Link(name="doc", url="dist/docs/index.html"),
+                Link(name="coverage", url="coverage/lcov-report/index.html"),
+                Link(name="bundle-analysis", url="dist/bundle-analysis.html"),
+                Link(name="html outputs", url="src/tests/.html-outputs/index.html")
+            ]),
             testConfig=TestStepConfig(
                 artifacts=[test_result, test_coverage, test_html_outputs]
+            ),
+            publishConfig=PublishConfig(
+                packagedArtifacts=['dist', 'docs', 'test-coverage', 'test-html-outputs']
             )
         )
         return await pipeline(config, context)
