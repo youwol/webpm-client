65b743c4b6bf22575083f5020428916c9d37e9bc
youwol-guillaume <greinisch@youwol.com>
2022-09-09 16:52:46 +0200

    :white_check_mark: add tests on 'pinDependencies' & 'patchUrl'

--- src/tests/rxjs.test.ts
+++ src/tests/rxjs.test.ts
@@ -120,3 +120,48 @@ test('install rxjs-test#6 & rxjs-test#7', async () => {
     expect(rxjs7).toBeTruthy()
     expect(rxjs).toEqual(rxjs7)
 })
+
+test('install rxjs-test#7 with pined dependencies @#6', async () => {
+    State.pinDependencies(['rxjs-test#6.5.5'])
+    const { rxjs, rxjs6, rxjs7 } = (await install({
+        modules: [
+            {
+                name: 'rxjs-test',
+                version: '7.5.5',
+            },
+        ],
+        aliases: {
+            rxjs: 'rxjs-test',
+            rxjs6: 'rxjs-test#6',
+            rxjs7: 'rxjs-test#7',
+        },
+    })) as any
+
+    expect(document.scripts).toHaveLength(1)
+    expect(rxjs).toBeTruthy()
+    expect(rxjs6).toBeTruthy()
+    expect(rxjs7).toBeFalsy()
+    expect(rxjs).toEqual(rxjs6)
+})
+
+test('install rxjs-test#7 with patched url', async () => {
+    const oldWarn = console.warn
+    console.warn = () => {
+        console.warn('A warning has been generated')
+    }
+    State.registerUrlPatcher(({ url }) => {
+        return url.replace('7.5.5', '6.5.5')
+    })
+    await install({
+        modules: [
+            {
+                name: 'rxjs-test',
+                version: '7.5.5',
+            },
+        ],
+    })
+
+    expect(window['rxjs-test_APIv6']).toBeTruthy()
+    expect(window['rxjs-test_APIv7']).toBeFalsy()
+    console.warn = oldWarn
+})
