33c3c3174f242715d64521b2f1e47bab076d71d7
youwol-guillaume <greinisch@youwol.com>
2022-09-12 11:11:52 +0200

    :wrench: update template.py and regenerate files - webpack.config is now ts

--- template.py
+++ template.py
@@ -1,15 +1,21 @@
+import shutil
 from pathlib import Path
 
 from youwol.pipelines.pipeline_typescript_weback_npm import Template, PackageType, Dependencies, \
     RunTimeDeps, generate_template
+from youwol_utils import parse_json
+
+folder_path = Path(__file__).parent
+
+pkg_json = parse_json(folder_path / 'package.json')
 
 template = Template(
-    path=Path(__file__).parent,
+    path=folder_path,
     type=PackageType.Library,
-    name="@youwol/cdn-client",
-    version="0.1.5-wip",
-    shortDescription="Library for dynamic npm's libraries installation from YouWol's CDN.",
-    author="greinisch@youwol.com",
+    name=pkg_json['name'],
+    version=pkg_json['version'],
+    shortDescription=pkg_json['description'],
+    author=pkg_json['author'],
     dependencies=Dependencies(
         runTime=RunTimeDeps(
             load={
@@ -29,3 +35,11 @@ template = Template(
     )
 
 generate_template(template)
+shutil.copyfile(
+    src=folder_path / '.template' / 'src' / 'auto-generated.ts',
+    dst=folder_path / 'src' / 'auto-generated.ts'
+)
+shutil.copyfile(
+    src=folder_path / '.template' / 'README.md',
+    dst=folder_path / 'README.md'
+)
