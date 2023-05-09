e06cf2eba08727d2071326dcaab16fd215d8e826
Julien DÉCHARNE <jdecharne@youwol.com>
2023-05-09 13:01:44 +0200

    👽️ Youwol modules renaming

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,8 +1,8 @@
-from youwol.environment import YouwolEnvironment
-from youwol.environment.models_project import IPipelineFactory, JsBundle, Artifact, FileListing, Link
+from youwol.app.environment import YouwolEnvironment
+from youwol.app.environment.models_project import IPipelineFactory, JsBundle, Artifact, FileListing, Link
 from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig, TestStepConfig, test_result, \
     test_coverage, PublishConfig
-from youwol_utils.context import Context
+from youwol.utils.context import Context
 
 test_html_outputs: Artifact = Artifact(
     id='test-html-outputs',
