5851e5042c2e335fed99a36bd3656119c0027d8e
youwol-guillaume <greinisch@youwol.com>
2023-05-12 17:39:17 +0200

    ✅ add `unit-tests.test.ts`

---
+++ src/tests/unit-tests.test.ts
@@ -0,0 +1,39 @@
+import { StateImplementation } from '../lib/state'
+import { getApiKey, getExpectedFullExportedSymbol } from '../lib/utils'
+
+function fakeInstalledModule(name, version) {
+    StateImplementation.exportedSymbolsDict[`${name}#${version}`] = {
+        symbol: name,
+        apiKey: getApiKey(version),
+    }
+    StateImplementation.registerImportedModules(
+        [
+            {
+                name,
+                version,
+            },
+        ],
+        window,
+    )
+    const symbol = getExpectedFullExportedSymbol(name, version)
+    window[symbol] = {
+        setup: { name, version, apiVersion: getApiKey(version) },
+    }
+}
+
+test('StateImplementation.isCompatibleVersionInstalled', () => {
+    fakeInstalledModule('@youwol/cdn-client', '1.2.0')
+    const tests = {
+        '1.1.0': true,
+        '1.3.0': false,
+        '0.10.0': false,
+        '0.0.10': false,
+    }
+    Object.entries(tests).map(([version, expected]) => {
+        const r = StateImplementation.isCompatibleVersionInstalled(
+            '@youwol/cdn-client',
+            version,
+        )
+        expect(r).toBe(expected)
+    })
+})
