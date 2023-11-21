3634438019347662d60684b1eb931aab0dd2884a
youwol-guillaume <greinisch@youwol.com>
2023-11-21 14:01:22 +0100

    🔧 `rx-vdom-config.ts` => set 'Prod' conf.

--- rx-vdom-config.ts
+++ rx-vdom-config.ts
@@ -1,7 +1,7 @@
 type AllTags = keyof HTMLElementTagNameMap
 export type Configuration = {
     TypeCheck: 'strict'
-    SupportedHTMLTags: 'Dev' extends 'Prod' ? AllTags : DevTags
+    SupportedHTMLTags: 'Prod' extends 'Prod' ? AllTags : DevTags
     WithFluxView: false
 }
 
